6fffd3486d227d65ac3fd373087412e860b64914
cotgyu <v123v123s@naver.com>
2020-04-22 18:17:17 +0900

    Add SpringBootServletInitializer 설정 추가

--- src/main/java/com/edu/EduApplication.java
+++ src/main/java/com/edu/EduApplication.java
@@ -2,6 +2,8 @@ package com.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -9,19 +11,30 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class EduApplication {
+public class EduApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EduApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduApplication.class, args);
 	}
 
-	@Bean
-	public InternalResourceViewResolver setupViewResolver() {
 
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	// 필요 없는 설정으로 보임
+//
+//	@Bean
+//	public InternalResourceViewResolver setupViewResolver() {
+//
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//
+//		resolver.setPrefix("/WEB-INF/jsp/");
+//		resolver.setSuffix(".jsp");
+//		return resolver;
+//	}
+//
 
-		resolver.setPrefix("/WEB-INF/jsp/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
 }
